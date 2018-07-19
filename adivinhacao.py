e2dd18933dbb713866af4878bc1c12437988d513
Felipe Silva Ribeiro <felipe.ribeiro@fortbrasil.com.br>
2018-07-19 13:07:21 -0300

    Nivis de dificuldade

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,11 +1,25 @@
+import random
+
 print("*********************************")
 print("Bem vindo ao jogo de Adivinhação!")
 print("*********************************")
 
-numero_secreto = 42
-total_tentativas = 3
-rodada = 1
+numero_secreto = random.randrange(1,101)
+total_tentativas = 0
+pontos = 1000
 
+
+print("Qual o nivel de dificuldade ? ")
+print("(1) Facil (2) Médio (3) Dificil")
+
+nivel = int(input("Defina o nivel: "))
+
+if (nivel == 1):
+    total_tentativas = 20
+elif (nivel==2):
+    total_tentativas = 10
+else:
+    total_tentativas = 5
 
 for rodada in range(1,total_tentativas + 1):
 
@@ -20,15 +34,12 @@ for rodada in range(1,total_tentativas + 1):
         print("Você deve digitar um número entre 1 e 100")
         continue
 
-
-    print("Voce digitou", chute)
-
     acertou = chute == numero_secreto
     maior = chute > numero_secreto
     menor = chute < numero_secreto
 
     if (acertou):
-        print("Voce acertou o numero !!")
+        print("Voce acertou o numero e fez {} pontos!!".format(pontos))
         break
     else:
         if (maior):
@@ -36,6 +47,8 @@ for rodada in range(1,total_tentativas + 1):
         elif (menor):
             print("Você errou ! seu numero menor que o numero secreto")
 
+        pontos_perdidos = abs(numero_secreto - chute)
+        pontos = pontos - pontos_perdidos
 
 print("\n")
 print("**********************************")
