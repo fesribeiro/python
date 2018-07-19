1292023e6cd7c3c8e55d3bd81b5f56825ba86680
Felipe Ribeiro <fesribeiro0@gmail.com>
2018-07-18 21:56:24 -0300

    Colocando número aleatorios

--- adivinhacao.py
+++ adivinhacao.py
@@ -7,13 +7,19 @@ total_tentativas = 3
 rodada = 1
 
 
-while (rodada <= total_tentativas):
+for rodada in range(1,total_tentativas + 1):
 
     print("Tentativa {} de {}".format(rodada,total_tentativas))
 
-    chute_str = input("Digite o seu numero: ")
+    chute_str = input("Digite um número entre 1 e 100: ")
 
     chute = int(chute_str)
+
+
+    if(chute < 1 or chute > 100):
+        print("Você deve digitar um número entre 1 e 100")
+        continue
+
 
     print("Voce digitou", chute)
 
@@ -23,15 +29,15 @@ while (rodada <= total_tentativas):
 
     if (acertou):
         print("Voce acertou o numero !!")
+        break
     else:
         if (maior):
             print("Você errou ! seu numero maior que o numero secreto")
         elif (menor):
             print("Você errou ! seu numero menor que o numero secreto")
 
-    rodada = rodada + 1
 
-
+print("\n")
 print("**********************************")
 print("Fim de jogo")
 print("**********************************")
