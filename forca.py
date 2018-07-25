9b3a95801a88a9c0c067446d01ff9c2dc8a1cc5d
Felipe Silva Ribeiro <felipe.ribeiro@fortbrasil.com.br>
2018-07-25 13:26:49 -0300

    Tratando as entradas do usuário no jogo de forca

--- forca.py
+++ forca.py
@@ -6,4 +6,33 @@ def jogar():
     print("************************************")
     print("************************************")
 
+    palavra_secreta = "banana"
+
+    enforcou = False
+    acertou = False
+
+    #enquato(true)
+    while(not enforcou and not acertou):
+
+        chute = input("Qual a letra ? ")
+
+        chute = chute.strip()
+
+        index = 0
+
+        for letra in palavra_secreta:
+            if (chute.upper() == letra.upper()):
+                print("Encontrei a letra {} na posição {}".format(letra, index))
+
+            index = index + 1
+
+
+        print("Jogando...")
+
+
+    print("Fim do jogo")
+
+
+
+if(__name__ == "__main__"):
     jogar()
