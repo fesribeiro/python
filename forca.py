8e487f982e212466dc53d51fdc00fd6f79764e3e
Felipe Silva Ribeiro <felipe.ribeiro@fortbrasil.com.br>
2018-07-31 13:30:13 -0300

    Manipulando o loop com acertou e enforcou

--- forca.py
+++ forca.py
@@ -6,12 +6,15 @@ def jogar():
     print("************************************")
 
 
-    palavra_secreta = "banana"
+    palavra_secreta = "banana".upper()
+
 
     letras_acertadas = ["_", "_", "_", "_", "_", "_"]
 
     enforcou = False
     acertou = False
+
+    erros = 0
 
     print("\n", letras_acertadas, "\n")
 
@@ -20,18 +23,29 @@ def jogar():
 
         chute = input("Qual a letra ? ")
 
-        chute = chute.strip()
+        chute = chute.strip().upper()
 
-        index = 0
+        
 
-        for letra in palavra_secreta:
-            if (chute.upper() == letra.upper()):
-                letras_acertadas[index] = letra
+        if(chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if (chute == letra):
+                    letras_acertadas[index] = letra
 
-            index = index + 1
+                index += 1
+        else:
+            erros += 1
 
+        enforcou = erros == 6 
+        acertou = "_" not in letras_acertadas
 
         print("\n", letras_acertadas, "\n")
+
+    if (acertou):
+        print("Você ganhou !!")
+    else:
+        print("Você perdeu !!")
 
 
     print("Fim do jogo")
