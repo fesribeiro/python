2ee14c1826d300c98e1c3b5cf1c4354bb2dc9fa6
Felipe Silva Ribeiro <felipe.ribeiro@fortbrasil.com.br>
2018-08-01 13:02:59 -0300

    Lendo o nome da fruta em um arquivo e usando a bilbioteca random

--- forca.py
+++ forca.py
@@ -1,3 +1,5 @@
+import random
+
 def jogar():
     print("************************************")
     print("************************************")
@@ -6,10 +8,22 @@ def jogar():
     print("************************************")
 
 
-    palavra_secreta = "banana".upper()
+    arquivo = open("palavras.txt", "r")
+    palavras = []
 
 
-    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+
+    arquivo.close()
+
+    numero = random.randrange(0,len(palavras))
+
+    palavra_secreta = palavras[numero].upper()
+
+
+    letras_acertadas = ["_" for letra in palavra_secreta]
 
     enforcou = False
     acertou = False
