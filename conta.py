ce1581902440247984e4869a27cb5966061e3aa9
Felipe Silva Ribeiro <felipe.ribeiro@fortbrasil.com.br>
2018-08-14 12:23:41 -0300

    Atributos privados

--- conta.py
+++ conta.py
@@ -1,17 +1,23 @@
-
-
-
 class Conta:
-
-
     def __init__(self, numero, titular, saldo, limite):
         print("Construindo um objeto...{}".format(self))
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
+
+    
+    def extrato(self):
+        print("Seu saldo é de {} no nome do titular {}".format(self.__saldo, self.__titular))
+
+    def saca(self, valor):
+        self.__saldo -= valor
+
+    def depositar(self, valor):
+        self.__saldo += valor
+
+    def transfere(self, valor, origem, destino):
+        origem.saca(valor)
+        destino.depositar(valor)
 
 
-
-conta = Conta(123, "Felipe", 55.0, 1000.0)
-conta2 = Conta(123, "Victor", 55.0, 1000.0)
