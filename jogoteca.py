007c61c444ad4220d97f24ef3b5af6212d5857b3
Felipe Silva Ribeiro <felipe.ribeiro@fortbrasil.com.br>
2018-08-21 12:44:04 -0300

    Flask part 2

--- jogoteca.py
+++ jogoteca.py
@@ -1,13 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+class Jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+
+
 @app.route('/inicio/')
 def ola():
-    return '<h1>Teste</h1>'
+    jogo1 = Jogo('Super Mário', 'Ação', 'Super Nitendo')
+    jogo2 = Jogo('Pokemon Gold', 'RPG', 'GameBoy')
+    lista = [jogo1, jogo2]
+    return render_template('lista.html', titulo= 'Jogos')
 
+
+#app.run(host='localhost', port=8080) -> Rodar em uma porta e ip diferente
 app.run()
-
-
-
-
