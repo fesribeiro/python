7e64c14f46b67fe1361cf56b602865c78ab1af3c
Felipe Ribeiro <fesribeiro0@gmail.com>
2018-08-21 23:31:49 -0300

    Inciando formulário flask

--- jogoteca.py
+++ jogoteca.py
@@ -1,21 +1,22 @@
 from flask import Flask, render_template
-
 app = Flask(__name__)
 
 class Jogo:
+
     def __init__(self, nome, categoria, console):
         self.nome = nome
         self.categoria = categoria
         self.console = console
 
 
-@app.route('/inicio/')
-def ola():
-    jogo1 = Jogo('Super Mário', 'Ação', 'Super Nitendo')
-    jogo2 = Jogo('Pokemon Gold', 'RPG', 'GameBoy')
-    lista = [jogo1, jogo2]
-    return render_template('lista.html', titulo= 'Jogos')
+@app.route('/')
+def index():
+    Jogo1 = Jogo('Super Mario', 'Ação', 'Nitendo')
+    Jogo2 = Jogo('GTA', 'Ação', 'Xbox')
+    Jogo3 = Jogo('Mortal Combat', 'Luta', 'Nitendo')
+    lista = [Jogo1, Jogo2, Jogo3]
+    return render_template('lista.html', titulo='Jogos', jogos=lista)
 
-
-#app.run(host='localhost', port=8080) -> Rodar em uma porta e ip diferente
-app.run()
+if __name__ == '__main__':
+  app.run(host='127.0.0.1', port=8000, debug=True)
+ 
