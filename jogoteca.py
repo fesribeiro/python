d1a96f0a5be49c6a64cb9a492a1738144a3beab2
Felipe Silva Ribeiro <felipe.ribeiro@fortbrasil.com.br>
2018-08-22 12:46:19 -0300

    Form

--- jogoteca.py
+++ jogoteca.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 class Jogo:
@@ -9,13 +9,28 @@ class Jogo:
         self.console = console
 
 
+Jogo1 = Jogo('Super Mario', 'Ação', 'Nitendo')
+Jogo2 = Jogo('GTA', 'Ação', 'Xbox')
+Jogo3 = Jogo('Mortal Combat', 'Luta', 'Nitendo')
+lista = [Jogo1, Jogo2, Jogo3]
+
+
 @app.route('/')
 def index():
-    Jogo1 = Jogo('Super Mario', 'Ação', 'Nitendo')
-    Jogo2 = Jogo('GTA', 'Ação', 'Xbox')
-    Jogo3 = Jogo('Mortal Combat', 'Luta', 'Nitendo')
-    lista = [Jogo1, Jogo2, Jogo3]
     return render_template('lista.html', titulo='Jogos', jogos=lista)
+
+@app.route('/novo/')
+def novo():
+    return render_template('novo.html', titulo='Novo Jogo')
+
+@app.route('/criar', methods=['POST',])
+def criar():
+    nome = request. form['nome']
+    categoria = request. form['categoria']
+    console = request. form['console']
+    jogo = Jogo(nome, categoria, console)
+    lista.append(jogo)
+    return render_template('lista.html', titulo='Jogo', jogos=lista)
 
 if __name__ == '__main__':
   app.run(host='127.0.0.1', port=8000, debug=True)
