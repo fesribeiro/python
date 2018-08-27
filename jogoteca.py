dcf1a9e5140b425ac34f118dfe0953b80deb5bc4
Felipe Silva Ribeiro <felipe.ribeiro@fortbrasil.com.br>
2018-08-27 11:58:12 -0300

    Materialize

--- jogoteca.py
+++ jogoteca.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, session, flash
+from flask import Flask, render_template, request, redirect, session, flash, url_for
 app = Flask(__name__)
 app.secret_key = 'Felipe'
 
@@ -18,10 +18,13 @@ lista = [Jogo1, Jogo2, Jogo3]
 
 @app.route('/')
 def index():
+    
     return render_template('lista.html', titulo='Jogos', jogos=lista)
 
 @app.route('/novo/')
 def novo():
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        return redirect(url_for('login', proxima=url_for('novo')))
     return render_template('novo.html', titulo='Novo Jogo')
 
 @app.route('/criar', methods=['POST',])
@@ -31,23 +34,31 @@ def criar():
     console = request. form['console']
     jogo = Jogo(nome, categoria, console)
     lista.append(jogo)
-    return redirect('/')
+    return redirect(url_for('index'))
 
-@app.route('/autenticar/', methods=['POST',])
+@app.route('/autenticar', methods=['POST',])
 def autenticar():
     if 'mestra' == request.form['senha']:
         session['usuario_logado'] = request.form['usuario']
-        flash(request.form['usuario'] + 'Logou com sucesso !')
-        return redirect('/')
+        flash(request.form['usuario'] + ' logou com sucesso !')
+        proxima_pagina = request.form['proxima']
+        return redirect(proxima_pagina)
     else:
-        flash('Usuário ' + request.form['usuario'] + 'não logou !')
-        return redirect('/login')
+        flash('Usuário ' + request.form['usuario'] + ' não logou !')
+        return redirect(url_for('login'))
 
-@app.route('/login', methods=['POST',])
+@app.route('/login')
 def login():
-    return render_template('login.html')
+    proxima = request.args.get('proxima')
+    return render_template('login.html', proxima=proxima)
 
+
+@app.route('/logout')
+def logout():
+    session['usuario_logado'] = None
+    flash('nenhum usuário logado !')
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
-  app.run(host='127.0.0.1', port=8000, debug=True)
+  app.run(host='192.168.40.60', port=8000, debug=True)
  
