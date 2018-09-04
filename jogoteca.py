cefb057e0f05ef5b1124209d56f32774dfcaa7e9
Felipe Ribeiro <fesribeiro0@gmail.com>
2018-09-03 22:42:37 -0300

    Upload

--- jogoteca.py
+++ jogoteca.py
@@ -2,14 +2,11 @@ from flask import Flask, render_template, request, redirect, session, flash, url
 from dao import JogoDao, UsuarioDao
 from models import Jogo, Usuario
 import pymysql
+import os
+
 app = Flask(__name__)
 app.secret_key = 'Felipe'
-#host = "0.0.0.0"
-#user = "root"
-#passwd = ""
-#db = 'jogoteca'
-#port = 3306
-
+app.config['UPLOAD_PATH'] = os.path.dirname(os.path.abspath(__file__)) + '/upload'
 db = pymysql.connect(user='root',passwd='', host='localhost', port=3306)
 
 jogo_dao = JogoDao(db)
@@ -42,18 +39,33 @@ def editar(id):
     jogo = jogo_dao.busca_por_id(id)
     return render_template('editar.html', titulo='Alterar jogo', jogo=jogo)
 
+@app.route('/deletar/<int:id>')
+def deletar(id):
+    jogo_dao.deletar(id)
+    flash("Jogo excluido com sucesso !")
+    return redirect(url_for('index'))
+
+
 @app.route('/criar', methods=['POST',])
 def criar():
     nome = request. form['nome']
     categoria = request. form['categoria']
     console = request. form['console']
     jogo = Jogo(nome, categoria, console)
-    jogo_dao.salvar(jogo)
+    jogo = jogo_dao.salvar(jogo)
+    arquivo = request.files['arquivo']
+    upload_path = app.config['UPLOAD_PATH']
+    arquivo.save('upload/' + arquivo.filename)
     return redirect(url_for('index'))
 
 @app.route('/atualizar', methods=['POST',])
 def atualizar():
-    pass
+    nome = request. form['nome']
+    categoria = request. form['categoria']
+    console = request. form['console']
+    jogo = Jogo(nome,categoria, console, id=request.form['id'])
+    jogo_dao.salvar(jogo)
+    return redirect(url_for('index'))
 
 @app.route('/autenticar', methods=['POST',])
 def autenticar():
@@ -65,7 +77,7 @@ def autenticar():
             proxima_pagina = request.form['proxima']
         return redirect(proxima_pagina)
     else:
-        flash('Usuário ' + request.form['usuario'] + 'não logou !')
+        flash('Usuário ' + request.form['usuario'] + ' não logou !')
         return redirect(url_for('login'))
 
 @app.route('/login')
@@ -77,9 +89,9 @@ def login():
 def logout():
     session['usuario_logado'] = None
     flash('Nenhum usuário logado!')
-    return redirect(url_for('index'))
+    return redirect(url_for('login'))
 
 
 if __name__ == '__main__':
-  app.run(host='localhost', port=8000, debug=True)
+  app.run(host='192.168.1.3', port=8000, debug=True)
  
